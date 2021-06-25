02f75da646250b8e7766115f9b7e55d9df7de57a
Zek_ken <Zek_ken@criptext.com>
2021-06-25 14:14:12 +0545

    First commit

---
+++ driver.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from cloudscraper import CloudScraper
+import os
+
+driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
+url = input("Enter the Url: ")
+folder = url.split("/")[-2]
+
+def imagedown(url, folder):
+  scraper = CloudScraper.create_scraper()
+  driver.get(url)
+  source = driver.page_source
+  soup = BeautifulSoup(source, "html.parser")
+  images = soup.find_all("amp-img")
+  try:
+    os.mkdir(os.path.join(os.getcwd(), folder))
+  except:
+    pass
+  os.chdir(os.path.join(os.getcwd(), folder))
+  for image in images:
+    link = image["src"]
+    name = link.split("/")
+    with open(f"{name[-1]}", "wb") as f:
+      f.write(scraper.get(f"{link}").content)
+      print("Writing: ", name[-1])
+
+imagedown(url, folder)
+driver.quit()
