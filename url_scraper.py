4a15588f12d196ba7184933e806e563fbc9f9e2e
niromru <emailr47@gmail.com>
2022-03-09 00:40:12 +0545

    creates cbz and moves them to the Downloads folder

--- url_scraper.py
+++ url_scraper.py
@@ -1,10 +1,12 @@
 from cloudscraper import CloudScraper
 from bs4 import BeautifulSoup
-import os, warnings, re
+from re import search as rsearch
+from shutil import rmtree, move
+import os, zipfile
 
 url = input("Enter the url: ")
 folder = url.split("/")[-2]
-warnings.simplefilter("ignore")
+basedir = os.getcwd()
 
 def url_change(url):
   url_split = url.split("/")
@@ -18,7 +20,7 @@ def imagedown(url, folder):
     source = scraper.get(url_change(url)).text
   else:
     source = scraper.get(url).text
-  soup = BeautifulSoup(source)
+  soup = BeautifulSoup(source, "lxml")
   images = soup.find_all("amp-img")
   try:
     os.mkdir(os.path.join(os.getcwd(), folder))
@@ -28,11 +30,36 @@ def imagedown(url, folder):
   for image in images:
     link = image["src"]
     name = link.split("/")
-    if re.search(r"jpg|jpeg|png", link.split(".")[-1]):
+    if rsearch(r"jpg|jpeg|png", link.split(".")[-1]):
       with open(f"{name[-1]}", "wb") as f:
         f.write(scraper.get(f"{link}").content)
         print("Writing: ", name[-1])
     else:
       print("This url is not valid!")
 
+def prepare_zip(folder):
+  os.chdir(basedir)
+  new_file = folder + '.cbz'
+  zipf = zipfile.ZipFile(new_file, 'w', zipfile.ZIP_DEFLATED)
+  for dir_path, dir_names, files in os.walk(folder):
+    f_path = dir_path.replace(dir_path, '')
+    f_path = f_path and f_path + os.sep
+    for file in files:
+      zipf.write(os.path.join(dir_path, file), f_path + file)
+  zipf.close()
+  print("File Created successfully..")
+  return new_file
+
+def move_dls(folder):
+  os.chdir(basedir)
+  try:
+    os.chdir("Downloads")
+  except FileNotFoundError:
+    os.mkdir("Downloads")
+    os.chdir("Downloads")
+  move(basedir + "/" + folder+ ".cbz", basedir + "/Downloads")
+  rmtree(os.path.join(basedir, folder))
+  
 imagedown(url, folder)
+prepare_zip(folder)
+move_dls(folder)
